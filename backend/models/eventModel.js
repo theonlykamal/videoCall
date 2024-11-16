dbcf5e78bbb146bd7e1f81e27d92f18288412c65
theonlykamal <krishnakamal.code@proton.me>
2024-11-16 18:45:10 +0530

    redoply

--- backend/models/eventModel.js
+++ backend/models/eventModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const messageModel = mongoose.Schema({
+const eventModel = mongoose.Schema({
     user : {
         type : mongoose.Schema.Types.ObjectId,
         ref: "User"
